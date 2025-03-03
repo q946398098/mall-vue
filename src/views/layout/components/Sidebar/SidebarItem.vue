9082a706b81c8734dc15d46e0b798ef5b22610a5
Martin.Hu <Martin.Hu@ttigroup.com>
2025-03-03 14:15:01 +0800

    new menu

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,37 +1,66 @@
 <template>
-    <template v-for="route in routes" :key="route.path">
-      <!-- 菜单分组 -->
-      <el-sub-menu v-if="route.children && route.hidden === false && route.children.length > 0" :index="route.path">
-        <template #title> <!--插槽-->
-          <el-icon>
-            <svg-icon v-if="route.meta && route.meta.icon" :icon-class="route.meta.icon"/>
-          </el-icon>
-          <span>{{ route.meta.title }}</span>
-        </template>
-        <template v-for="child in route.children" :key="child.path">
-          <router-link v-if="child.hidden === false" :to="child.path">
-            <el-menu-item :index="child.path">
-              <el-icon>
-                <svg-icon v-if="child.meta && child.meta.icon" :icon-class="child.meta.icon"/>
-              </el-icon>
-              <span>{{ child.meta.title }}</span>
-            </el-menu-item>
-          </router-link>
-        </template>
-      </el-sub-menu>
-      <!-- 单个菜单项 -->
-      <el-menu-item v-else-if="!route.hidden" :index="route.path">
-         <el-icon>
-        <svg-icon v-if="route.meta && route.meta.icon" :icon-class="route.meta.icon"/>
-         </el-icon>
-        <span>{{ route.meta.title }}</span>
-      </el-menu-item>
+    <template v-for="(route,index) in routes">
+
+      <!-- 有子菜單 -->
+      <template v-if="route.children && route.hidden === false && route.children.length > 0">
+        <el-sub-menu  :index="index">
+
+          <template #title> <!--插槽-->
+            <el-icon>
+              <svg-icon v-if="route.meta && route.meta.icon" :icon-class="route.meta.icon"/>
+            </el-icon>
+            <span>{{ route.meta.title }}</span>
+          </template>
+
+          <template v-for="(child,child_index) in route.children">
+            <template v-if="child.children && child.hidden === false && child.children.length > 0">
+              <SidebarItem :routes="child.children"/>
+            </template>
+            <template v-if="child.path.startsWith('http')">
+              <a :href="child.path" target="_blank">
+                <el-menu-item :index="index +  '-' + child_index">
+                  <el-icon>
+                    <svg-icon v-if="child.meta && child.meta.icon" :icon-class="child.meta.icon"></svg-icon>
+                  </el-icon>
+                  <span>{{ child.meta.title }}</span>
+                </el-menu-item>
+              </a>
+            </template>
+            <template v-if="!child.path.startsWith('http') && child.hidden === false">
+              <router-link  :to="child.path">
+                <el-menu-item :index="index +  '-' + child_index">
+                  <el-icon>
+                    <svg-icon v-if="child.meta && child.meta.icon" :icon-class="child.meta.icon"/>
+                  </el-icon>
+                  <span>{{ child.meta.title }}</span>
+                </el-menu-item>
+              </router-link>
+            </template>
+          </template>
+
+        </el-sub-menu>
+      </template>
+
+      <!-- 沒有子菜單 -->
+      <template v-else-if="!route.hidden">
+        <router-link :to="route.path">
+          <el-menu-item :index="index">
+            <el-icon>
+              <svg-icon :icon-class="route.meta.icon"/>
+            </el-icon>
+            <span>{{ route.meta.title }}</span>
+          </el-menu-item>
+        </router-link>
+      </template>
+
     </template>
 </template>
 
 <script setup>
 
 import {computed} from "vue";
+import SidebarItem from './SidebarItem.vue'; // 确保递归组件的引入
+
 
 const props = defineProps({
   routes: {
